79fbaa6fc0d727cc6737057afd3ef342905c5b63
silvia.hth <silvia.hth@gmail.com>
2021-11-25 17:21:44 +0100

    feat: aggiunta immagine che cambia nella hero ogni 3 s

--- js/script.js
+++ js/script.js
@@ -129,3 +129,19 @@ function setCartProductsNum() {
     localStorage.clear();
     setCartProductsNum();
   });
+
+  // cambio immagine hero
+
+ const imagesHero = ["https://images.unsplash.com/photo-1523199455310-87b16c0eed11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+ "https://images.unsplash.com/photo-1490481651871-ab68de25d43d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+ "https://images.unsplash.com/photo-1560243563-062bfc001d68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+ "https://images.unsplash.com/photo-1445205170230-053b83016050?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"]
+
+
+ const heroImg = document.querySelector(".overlay");
+ let image = 0
+
+ setInterval(function(){
+       image> 2 ? image= 0 : image +=1;
+       heroImg.style.backgroundImage =`url( ${imagesHero[image]})`;
+     },3000)
